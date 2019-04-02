486ac6990cb005fd3443213e72c7c6b25f1d22c4
tuyuri6ka <47937296+tuyuri6ka@users.noreply.github.com>
2019-04-03 01:04:19 +0900

    【追加】showページの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,9 @@ class PostsController < ApplicationController
   def index
     @posts= Post.all
   end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  
   get 'posts/index'
   get "/top"  => "home#top"
   get "/about" => "home#about"
   get "/posts/index" => "posts#index"
+  
+  #詳細ページに遷移
+  get "/posts/:id/show" => "posts#show"
 
   get "/" => "home#top"
 
