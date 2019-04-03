5dcd8436ad677005038ee3d7020413f5097f3fa0
tuyuri6ka <47937296+tuyuri6ka@users.noreply.github.com>
2019-04-03 21:20:39 +0900

    【作成】users_controllerの整備とroutesの整理を行いました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    
+    #name,email,passwordにバリデーション（空白を許さない）を追加
+    validates :name,{presence:true}
+    validates :email,{presence:true, uniqueness:true}
+    validates :password,{presence:true}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,26 @@
 Rails.application.routes.draw do
-  
+
   #home_controller
   get "/top"  => "home#top"
   get "/about" => "home#about"
+  
+  #user_controller
+  get "users/index" => "users#index"
+  get "users/new" => "users#new"
+  post "users/create" => "users#new"
+  post "users/:id/edit" => "users#edit"
+  get "users/:id/show" => "users#show"
+  post "users/:id/update" => "users#update"
+  post "users/:id/destroy" => "users#destroy"
 
   #posts_controller
-  get "/posts/new" => "posts#new" 
   get "/posts/index" => "posts#index"
+  get "/posts/new" => "posts#new" 
   post "/posts/create" => "posts#create"
-  post "/posts/:id/update" => "posts#update"
   get "/posts/:id/edit" => "posts#edit"
-  post "/posts/:id/destroy" => "posts#destroy"
   get "/posts/:id/show" => "posts#show"
+  post "/posts/:id/update" => "posts#update"
+  post "/posts/:id/destroy" => "posts#destroy"
 
   #root
   get "/" => "home#top"
