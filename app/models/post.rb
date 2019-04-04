7c03dee684727de319e4085061a3c4df17c34100
tuyuri6ka <47937296+tuyuri6ka@users.noreply.github.com>
2019-04-04 23:46:08 +0900

    【追加】post.rbにuserメソッドを追加し、showアクションを編集

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ApplicationRecord
     validates :title,{presence: true, length:{maximum: 140}}    #最大文字数は特に意味はないが設定
     validates :author,{presence: true}                          #著者で参考リンクを飛ばせると嬉しい。
 
+    #@post.userにより、postした投稿のuser情報を取得するメソッド
+    def user
+        return User.find_by(id: self.user_id)
+
 end
